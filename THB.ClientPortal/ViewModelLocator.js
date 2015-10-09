7360cb28fbded86402dad5e3d8107d0f6dd2eca2
sandsmi <sandra.smietana@onet.eu>
2015-10-09 11:23:16 +0200

    Added new Javascript files

---
+++ THB.ClientPortal/ViewModelLocator.js
@@ -0,0 +1,66 @@
+﻿var ViewModelLocator = (function () {
+    var self = {};
+
+    var bindings = [];
+
+    self.Register = function (binding) {
+        bindings.push(binding);
+    }
+
+    self.Get = function (path) {
+        var binding = bindings.find(function (element) {
+            return element.path != undefined && element.path == path;
+        });
+
+        if (binding == undefined)
+            return undefined;
+
+        return binding.viewModel();
+    }
+
+    return self;
+})();
+
+ViewModelLocator.Register({
+    path: '/Home/Index',
+    viewModel: function () {
+        return new TaskListModel([
+            { name: "test", number: "1", date: "02.08.2015", progress: "50%" },
+            { name: "another test", number: "2", date: "05.09.2015", progress: "100%" }
+        ]);
+    }
+});
+
+ViewModelLocator.Register({
+    path: '/Home/Index/',
+    viewModel: function () {
+        return new TaskListModel([
+            { name: "test", number: "1", date: "02.08.2015", progress: "50%" },
+            { name: "another test", number: "2", date: "05.09.2015", progress: "100%" }
+        ]);
+    }
+})
+
+ViewModelLocator.Register({
+    path: '/#Zlecenia',
+    viewModel: function () {
+        return new TaskListModel([
+            { name: "test", number: "1", date: "02.08.2015", progress: "50%" },
+            { name: "another test", number: "2", date: "05.09.2015", progress: "100%" }
+        ]);
+    }
+})
+
+ViewModelLocator.Register({
+    path: '/#Dodaj',
+    viewModel: function () {
+        return new TaskViewModel();
+    }
+})
+
+ViewModelLocator.Register({
+    path: '/Home/Index#Dodaj',
+    viewModel: function () {
+        return new TaskViewModel();
+    }
+})
---
+++ THB.ClientPortal/knockoutgrid.js
@@ -0,0 +1,20 @@
+﻿var TaskListModel = function (tasklist) {
+    var self = this;
+    self.tasklist = ko.observableArray(tasklist);
+
+    self.addTasks = function () {
+        self.tasklist.push({
+            name: "",
+            number: "",
+            date: "",
+            progress: ""
+        });
+    };
+
+    self.save = function (form) {
+        ko.utils.postJson($("form")[0], self.tasklist);
+    };
+
+    self.viewModelName = "Zlecenia";
+    self.viewName = "Zlecenia";
+};
