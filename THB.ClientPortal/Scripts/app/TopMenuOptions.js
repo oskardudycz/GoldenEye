3618892c8d4de245f9f6da207add5378da116e31
sandsmi <sandra.smietana@onet.eu>
2015-11-05 16:02:26 +0100

    Changed the top right menu

---
+++ THB.ClientPortal/Scripts/app/TopMenuOptions.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    if (authManager.getToken()) {
+        $("#notLoggedIn").hide();
+        $("#loggedIn").show();
+    }
+    else if (!authManager.getToken()) {
+        $("#loggedIn").hide();
+        $("#notLoggedIn").show();
+    }
+});
