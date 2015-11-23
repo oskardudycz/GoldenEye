6b6f75dba0a5d5a1bd0725c321fa380d6e5e3d72
Oskar Dudycz <o.dudycz@lgbs.pl>
2015-11-23 16:19:19 +0100

    Added missing files

---
+++ Backend.Core/Repository/IReadonlyRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+using Shared.Core;
+
+namespace Backend.Core.Repository
+{
+    public interface IReadonlyRepository<out TEntity> :IDisposable where TEntity : class, IHasId
+    {
+        TEntity GetById(int id);
+        IQueryable<TEntity> GetAll();
+    }
+}
