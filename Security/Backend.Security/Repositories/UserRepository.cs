7b404561c67590288a51a755d0d093ed61d62278
Oskar Dudycz <o.dudycz@lgbs.pl>
2015-12-11 16:53:36 +0100

    Added missing files

---
+++ Security/Backend.Security/Repositories/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using GoldenEye.Backend.Core.Repository;
+using GoldenEye.Backend.Security.DataContext;
+using GoldenEye.Backend.Security.Model;
+
+namespace GoldenEye.Backend.Security.Repositories
+{
+    public class UserRepository : RepositoryBase<User>, IUserRepository
+    {
+        public UserRepository(IUserDataContext context) : base(context, context.Users)
+        {
+        }
+    }
+}
