458f06b54dddff7b51fc8ea82e2ad3a0f0185bfe
Oskar Dudycz <o.dudycz@lgbs.pl>
2015-12-12 15:01:42 +0100

    Try to copy HomeController

---
+++ src/SPA/Frontend.SPA.Web/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+
+namespace GoldenEye.Frontend.SPA.Web.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
