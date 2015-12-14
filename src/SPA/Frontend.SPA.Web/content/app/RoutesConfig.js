6a65f8484333cd11e1adf3709c73dd381769547c
Oskar Dudycz <o.dudycz@lgbs.pl>
2015-12-14 13:05:44 +0100

    Javascript Config

---
+++ src/SPA/Frontend.SPA.Web/content/app/RoutesConfig.js
@@ -0,0 +1,10 @@
+﻿routing.init({
+    loginComponentName: "login-nc",
+    defaultViewName: "Zlecenia",
+    mappings: [
+        { view: "Login", component: "login-nc" },
+        { view: "Zlecenia", component: "TaskList-nc" },
+        { view: "Dodaj", component: "AddTask-nc" },
+        { view: "Detale", component: "TaskDetail-nc" }
+    ]
+});
