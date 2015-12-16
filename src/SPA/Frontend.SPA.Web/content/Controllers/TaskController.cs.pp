75f3023591f106481c04a2fc857ff9ef41b06a32
Oskar Dudycz <o.dudycz@lgbs.pl>
2015-12-16 15:48:10 +0100

    Added Task Controller

---
+++ src/SPA/Frontend.SPA.Web/content/Controllers/TaskController.cs.pp
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+using GoldenEye.Backend.Business.Services;
+using GoldenEye.Frontend.Core.Web.Controllers;
+
+namespace $rootnamespace$.Controllers
+{
+    [Authorize]
+    public class TaskController : RestControllerBase<ITaskRestService, TaskDTO>
+    {
+        public TaskController()
+        {
+        }
+
+        public TaskController(ITaskRestService service)
+            : base(service)
+        {
+        }
+    }
+}
