ee7fa787dce355e0f55203164bf9f4932eff846d
Oskar Dudycz <o.dudycz@lgbs.pl>
2015-12-16 15:58:28 +0100

    Installed SPA Shared package

---
+++ sample/SPA/GoldenEye.SPA.Shared.Sample/Module.cs
@@ -0,0 +1,13 @@
+using GoldenEye.Shared.Core.Modules;
+using GoldenEye.Shared.Core.Modules.Attributes;
+
+[assembly: ProjectAssembly]
+namespace GoldenEye.SPA.Shared.Sample
+{
+    public class Module : ModuleBase
+    {
+        public override void Load()
+        {
+        }
+    }
+}
---
+++ sample/SPA/GoldenEye.SPA.Shared.Sample/Services/ITaskRestService.cs
@@ -0,0 +1,9 @@
+using GoldenEye.Shared.Core.Services;
+using GoldenEye.SPA.Shared.Sample.DTOs;
+
+namespace GoldenEye.SPA.Shared.Sample.Services
+{
+    public interface ITaskRestService: IRestService<TaskDTO>
+    {
+    }
+}
