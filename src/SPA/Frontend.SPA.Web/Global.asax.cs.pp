aace1f33fd28e025e7636de86dbe4c68b45d5b0d
Oskar Dudycz <o.dudycz@lgbs.pl>
2015-12-17 12:06:48 +0100

    Added global.asax and module

---
+++ src/SPA/Frontend.SPA.Web/Global.asax.cs.pp
@@ -0,0 +1,14 @@
+﻿using System.Web.Optimization;
+using GoldenEye.Frontend.Core.Web;
+
+namespace $rootnamespace$
+{
+    public class MvcApplication : WebApplication
+    {
+        protected override void OnBundleConfig()
+        {
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            base.OnBundleConfig();
+        }
+    }
+}
---
+++ src/SPA/Frontend.SPA.Web/Module.cs.pp
@@ -0,0 +1,16 @@
+﻿using $rootnamespace$;
+using GoldenEye.Shared.Core.Modules;
+using GoldenEye.Shared.Core.Modules.Attributes;
+using Microsoft.Owin;
+
+[assembly: ProjectAssembly]
+[assembly: OwinStartup(typeof(OwinBoostrapper))]
+namespace $rootnamespace$
+{
+    public class Module : ModuleBase
+    {
+        public override void Load()
+        {
+        }
+    }
+}
