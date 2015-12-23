caa44421c21b9fa1f151207805a47373c95d0775
Oskar Dudycz <o.dudycz@lgbs.pl>
2015-12-23 16:47:53 +0100

    Added multiple configuration build

---
+++ src/build.bat
@@ -0,0 +1,13 @@
+@echo Off
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+
+set version=
+if not "%PackageVersion%" == "" (
+   set version=-Version %PackageVersion%
+)
+
+REM Build
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild GoldenEye.proj /p:Configuration="Both" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
