940b641867eb218f5684ca387a550dc2e732fe30
Oskar Dudycz <o.dudycz@lgbs.pl>
2017-01-07 00:03:11 +0100

    Fixed Web.config.pp template

--- src/SPA/Frontend.SPA.Web/Web.config.pp
+++ src/SPA/Frontend.SPA.Web/Web.config.pp
@@ -113,7 +113,6 @@
         </assemblyBinding>
     </runtime>
     <entityFramework>
-        <defaultConnectionFactory type="System.Data.Entity.Infrastructure.SqlConnectionFactory, EntityFramework" />
         <providers>
             <provider invariantName="System.Data.SqlClient" type="System.Data.Entity.SqlServer.SqlProviderServices, EntityFramework.SqlServer" />
         </providers>
