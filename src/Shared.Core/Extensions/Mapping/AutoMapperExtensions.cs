fe41953bca2a10af707dea1d0130900a35e02d24
Oskar Dudycz <o.dudycz@lgbs.pl>
2017-01-10 23:34:58 +0100

    #11 - Build fix

--- src/Shared.Core/Extensions/Mapping/AutoMapperExtensions.cs
+++ src/Shared.Core/Extensions/Mapping/AutoMapperExtensions.cs
@@ -2,80 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using AutoMapper;
-using GoldenEye.Shared.Core.Extensions.Reflection;
 
 namespace GoldenEye.Shared.Core.Extensions.Mapping
 {
     public static class AutoMapperExtension
     {
-        public static IMappingExpression IgnoreAllNonExisting(
-          this IMappingExpression expression, Type sourceType, Type destinationType)
-        {
-            var existingMaps = Mapper.GetAllTypeMaps().First(x => x.SourceType == sourceType
-                                                                  && x.DestinationType == destinationType);
-
-            foreach (var property in existingMaps.GetUnmappedPropertyNames())
-            {
-                expression.ForMember(property, opt => opt.Ignore());
-            }
-            return expression;
-        }
-
-        public static IMappingExpression<TSource, TDestination> IgnoreAllNonExisting<TSource, TDestination>(
-          this IMappingExpression<TSource, TDestination> expression)
-        {
-            var sourceType = typeof(TSource);
-            var destinationType = typeof(TDestination);
-            var existingMaps = Mapper.GetAllTypeMaps().First(x => x.SourceType == sourceType
-                                                                  && x.DestinationType == destinationType);
-
-            foreach (var property in existingMaps.GetUnmappedPropertyNames())
-            {
-                expression.ForMember(property, opt => opt.Ignore());
-            }
-            return expression;
-        }
-
-        // TODO: finish implementing, some problems occured
-        //public static IMappingExpression<TSource, TDestination> IgnoreAllNonExistingWithDefaults<TSource, TDestination>(
-        //    this IMappingExpression<TSource, TDestination> expression)
-        //{
-        //    var sourceType = typeof(TSource);
-        //    var destinationType = typeof(TDestination);
-        //    var existingMaps = Mapper.GetAllTypeMaps().First(x => x.SourceType == sourceType
-        //                                                          && x.DestinationType == destinationType);
-
-        //    var defaultedTypes = new Type[] { typeof(Guid), typeof(DateTime), typeof(List<>) };
-
-        //    var destinationTypeProperties = destinationType
-        //        .GetInterfaces().SelectMany(x => x.GetProperties(BindingFlags.FlattenHierarchy | BindingFlags.Public | BindingFlags.Instance))
-        //        .Union(destinationType.GetProperties(BindingFlags.FlattenHierarchy | BindingFlags.Public | BindingFlags.Instance));
-
-        //    var unmappedProperties = existingMaps.GetUnmappedPropertyNames().ToDictionary(i => i, i => destinationTypeProperties.Single(x => x.Name == i).PropertyType);
-        //    var defaultedProperties = unmappedProperties.Where(i => defaultedTypes.Contains(i.Value));
-
-        //    foreach (var property in defaultedProperties)
-        //    {
-        //        expression.ForMember(property.Key, opt => opt.MapFrom(model => ObjectExtensions.GetDefault(property.Value, null)));
-        //    }
-
-        //    return IgnoreAllNonExisting(expression);
-        //}
-
-        public static IMappingExpression<TSource, TDestination> UseDestinationForAllNonExisting<TSource, TDestination>(
-        this IMappingExpression<TSource, TDestination> expression)
-        {
-            var sourceType = typeof(TSource);
-            var destinationType = typeof(TDestination);
-            var existingMaps = Mapper.GetAllTypeMaps().First(x => x.SourceType == sourceType
-                                                                  && x.DestinationType == destinationType);
-            foreach (var property in existingMaps.GetUnmappedPropertyNames())
-            {
-                expression.ForMember(property, opt => opt.UseDestinationValue());
-            }
-            return expression;
-        }
-
         public static TEntity MapEntity<TDataContract, TEntity>(TDataContract source, TEntity destination)
         {
             return (TEntity)Mapper.Map(source, destination, typeof(TDataContract), typeof(TEntity));
@@ -124,27 +55,6 @@ namespace GoldenEye.Shared.Core.Extensions.Mapping
         public static TDestination MapFrom<TSource, TDestination>(this TDestination @this, TSource from)
         {
             return Mapper.Map(from, @this);
-        }
-
-        public static void CreateMapsForSourceTypes(this IConfiguration configuration, Func<Type, bool> filter, Func<Type, Type> destinationType, Action<IMappingExpression, Type, Type> mappingConfiguration)
-        {
-            var typesInThisAssembly = ReflectionExtensions.GetTypesFromAllProjectAssemblies();
-            CreateMapsForSourceTypes(configuration, typesInThisAssembly.Where(filter), destinationType, mappingConfiguration);
-        }
-
-        public static void CreateMapsForSourceTypes(this IConfiguration configuration, IEnumerable<Type> typeSource, Func<Type, Type> destinationType, Action<IMappingExpression, Type, Type> mappingConfiguration)
-        {
-            foreach (var type in typeSource)
-            {
-                var destType = destinationType(type);
-                if (destType == null) continue;
-                var mappingExpression = configuration.CreateMap(type, destType);
-
-                mappingConfiguration(mappingExpression, type, destType);
-
-                var mappingExpression2 = configuration.CreateMap(destType, type);
-                mappingConfiguration(mappingExpression2, destType, type);
-            }
         }
     }
 }
