4ee56e17f9f9bb6993968022f633ddc5e064d59c
Oskar Dudycz <oskar.dudycz@gmail.com>
2017-09-21 21:19:50 +0200

    updated ddd registration

--- src/Core/Backend.Core.DDD/Registration/Registration.cs
+++ src/Core/Backend.Core.DDD/Registration/Registration.cs
@@ -8,7 +8,7 @@ namespace GoldenEye.Backend.Core.DDD.Registration
 {
     public static class Registration
     {
-        public static void UseCQRS(IServiceCollection services)
+        public static void UseCQRS(this IServiceCollection services)
         {
             services.AddScoped<IMediator, Mediator>();
             services.AddTransient<SingleInstanceFactory>(sp => t => sp.GetService(t));
@@ -18,7 +18,7 @@ namespace GoldenEye.Backend.Core.DDD.Registration
             services.AddTransient<IQueryBus, QueryBus>();
         }
         
-        public static void RegisterCommandHandler<TCommand, TCommandHandler>(IServiceCollection services)
+        public static void RegisterCommandHandler<TCommand, TCommandHandler>(this IServiceCollection services)
             where TCommand : ICommand
             where TCommandHandler : class, ICommandHandler<TCommand>
         {
@@ -27,7 +27,7 @@ namespace GoldenEye.Backend.Core.DDD.Registration
             services.AddTransient<ICommandHandler<TCommand>>(sp => sp.GetService<TCommandHandler>());
         }
 
-        public static void RegisterAsyncCommandHandler<TCommand, TCommandHandler>(IServiceCollection services)
+        public static void RegisterAsyncCommandHandler<TCommand, TCommandHandler>(this IServiceCollection services)
             where TCommand : ICommand
             where TCommandHandler : class, IAsyncCommandHandler<TCommand>
         {
@@ -36,7 +36,7 @@ namespace GoldenEye.Backend.Core.DDD.Registration
             services.AddTransient<IAsyncCommandHandler<TCommand>>(sp => sp.GetService<TCommandHandler>());
         }
 
-        public static void RegisterQueryHandler<TQuery, TResponse, TQueryHandler>(IServiceCollection services)
+        public static void RegisterQueryHandler<TQuery, TResponse, TQueryHandler>(this IServiceCollection services)
             where TQuery : IQuery<TResponse>
             where TQueryHandler : class, IQueryHandler<TQuery, TResponse>
         {
@@ -45,7 +45,7 @@ namespace GoldenEye.Backend.Core.DDD.Registration
             services.AddTransient<IQueryHandler<TQuery, TResponse>>(sp => sp.GetService<TQueryHandler>());
         }
 
-        public static void RegisterAsyncQueryHandler<TQuery, TResponse, TQueryHandler>(IServiceCollection services)
+        public static void RegisterAsyncQueryHandler<TQuery, TResponse, TQueryHandler>(this IServiceCollection services)
             where TQuery : IQuery<TResponse>
             where TQueryHandler : class, IAsyncQueryHandler<TQuery, TResponse>
         {
@@ -54,7 +54,7 @@ namespace GoldenEye.Backend.Core.DDD.Registration
             services.AddTransient<IAsyncQueryHandler<TQuery, TResponse>>(sp => sp.GetService<TQueryHandler>());
         }
 
-        public static void RegisterEventHandler<TEvent, TEventHandler>(IServiceCollection services)
+        public static void RegisterEventHandler<TEvent, TEventHandler>(this IServiceCollection services)
             where TEvent : IEvent
             where TEventHandler : class, IEventHandler<TEvent>
         {
@@ -63,7 +63,7 @@ namespace GoldenEye.Backend.Core.DDD.Registration
             services.AddTransient<IEventHandler<TEvent>>(sp => sp.GetService<TEventHandler>());
         }
 
-        public static void RegisterAsyncEventHandler<TEvent, TEventHandler>(IServiceCollection services)
+        public static void RegisterAsyncEventHandler<TEvent, TEventHandler>(this IServiceCollection services)
             where TEvent : IEvent
             where TEventHandler : class, IAsyncEventHandler<TEvent>
         {
