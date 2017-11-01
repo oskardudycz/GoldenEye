46608dca1a3426ac60abb352e774a182d286e31c
Oskar Dudycz <oskar.dudycz@gmail.com>
2017-11-01 18:58:33 +0100

    Updated test data

--- src/Security/Backend.Identity/Clients/Tests/TestClients.cs
+++ src/Security/Backend.Identity/Clients/Tests/TestClients.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using IdentityServer4;
 using IdentityServer4.Models;
 
 namespace GoldenEye.Backend.Identity.Clients.Tests
@@ -15,6 +16,22 @@ namespace GoldenEye.Backend.Identity.Clients.Tests
                     ClientSecrets = new List<Secret> {
                         new Secret("secret".Sha256())},
                     AllowedScopes = new List<string> { "goldenEyeAPI.read" }
+                },
+                new Client
+                {
+                    ClientId = "openIdConnectClient",
+                    ClientName = "Example Implicit Client Application",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "role",
+                        "goldenEyeAPI.write"
+                    },
+                    RedirectUris = new List<string> {"https://localhost:4430/signin-oidc"},
+                    PostLogoutRedirectUris = new List<string> {"https://localhost:4430"}
                 }
             };
         }
