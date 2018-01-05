78e50cbc0de2b386fa1a2029bc9e184856eb35cf
Oskar Dudycz <oskar.dudycz@gmail.com>
2018-01-05 07:52:09 +0100

    Removed obsolete spa sample

--- sample/SPA/GoldenEye.SPA.Business.Sample/Repository/ITaskRepository.cs
+++
@@ -1,9 +0,0 @@
-using GoldenEye.Backend.Core.Repository;
-using GoldenEye.SPA.Business.Sample.Entities;
-
-namespace GoldenEye.SPA.Business.Sample.Repository
-{
-    public interface ITaskRepository: IRepository<TaskEntity>
-    {
-    }
-}
