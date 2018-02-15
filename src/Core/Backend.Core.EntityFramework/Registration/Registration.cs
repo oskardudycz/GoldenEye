02d4deb2a74a19619aa012808980b234530f3eb1
Oskar Dudycz <oskar.dudycz@gmail.com>
2018-02-15 21:08:48 +0100

    Fixes #48 - Add possibility to register handlers and other classes with desired scope

--- src/Core/Backend.Core.EntityFramework/Registration/Registration.cs
+++ src/Core/Backend.Core.EntityFramework/Registration/Registration.cs
@@ -1,31 +1,34 @@
-﻿using GoldenEye.Backend.Core.Context;
+﻿using System;
+using GoldenEye.Backend.Core.Context;
 using GoldenEye.Backend.Core.Entity;
 using GoldenEye.Backend.Core.Registration;
+using GoldenEye.Shared.Core.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using System;
 
 namespace GoldenEye.Backend.Core.EntityFramework.Registration
 {
     public static class Registration
     {
-        public static void AddEFDataContext<TDbContext>(this IServiceCollection services, Action<IServiceProvider, DbContextOptionsBuilder> optionsAction)
+        public static void AddEFDataContext<TDbContext>(this IServiceCollection services, Action<IServiceProvider, DbContextOptionsBuilder> optionsAction, ServiceLifetime serviceLifetime = ServiceLifetime.Scoped)
             where TDbContext : DbContext
         {
-            services.AddDbContext<TDbContext>(optionsAction, ServiceLifetime.Scoped);
-            services.AddScoped(sp => new EFDataContext<TDbContext>(sp.GetService<TDbContext>()));
+            services.AddDbContext<TDbContext>(optionsAction, serviceLifetime);
+            services.Add(sp => new EFDataContext<TDbContext>(sp.GetService<TDbContext>()), serviceLifetime);
         }
-        public static void AddEFCRUDRepository<TDbContext, TEntity>(this IServiceCollection services)
+
+        public static void AddEFCRUDRepository<TDbContext, TEntity>(this IServiceCollection services, ServiceLifetime serviceLifetime = ServiceLifetime.Transient)
             where TDbContext : DbContext
             where TEntity : class, IEntity
         {
-            services.AddCRUDRepository<EFDataContext<TDbContext>, TEntity>();
+            services.AddCRUDRepository<EFDataContext<TDbContext>, TEntity>(serviceLifetime);
         }
-        public static void AddEFReadonlyRepository<TDbContext, TEntity>(this IServiceCollection services)
+
+        public static void AddEFReadonlyRepository<TDbContext, TEntity>(this IServiceCollection services, ServiceLifetime serviceLifetime = ServiceLifetime.Transient)
             where TDbContext : DbContext
             where TEntity : class, IEntity
         {
-            services.AddReadonlyRepository<EFDataContext<TDbContext>, TEntity>();
+            services.AddReadonlyRepository<EFDataContext<TDbContext>, TEntity>(serviceLifetime);
         }
     }
 }
---
+++ src/Core/Shared.Core/Extensions/DependencyInjection/RegistrationExtensions.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GoldenEye.Shared.Core.Extensions.DependencyInjection
+{
+    public static class RegistrationExtensions
+    {
+        //
+        // Summary:
+        //     Adds a transient service of the type specified in TService with an implementation
+        //     type specified in TImplementation using the factory specified in implementationFactory
+        //     to the specified Microsoft.Extensions.DependencyInjection.IServiceCollection.
+        //
+        // Parameters:
+        //   services:
+        //     The Microsoft.Extensions.DependencyInjection.IServiceCollection to add the service
+        //     to.
+        //
+        //   implementationFactory:
+        //     The factory that creates the service.
+        //
+        // Type parameters:
+        //   TService:
+        //     The type of the service to add.
+        //
+        //   TImplementation:
+        //     The type of the implementation to use.
+        //
+        // Returns:
+        //     A reference to this instance after the operation has completed.
+
+        public static IServiceCollection Add<TService, TImplementation>(this IServiceCollection services, Func<IServiceProvider, TImplementation> implementationFactory, ServiceLifetime serviceLifetime = ServiceLifetime.Transient)
+            where TService : class
+            where TImplementation : class, TService
+        {
+            switch (serviceLifetime)
+            {
+                case ServiceLifetime.Singleton:
+                    return services.AddSingleton<TService, TImplementation>(implementationFactory);
+
+                case ServiceLifetime.Scoped:
+                    return services.AddScoped<TService, TImplementation>(implementationFactory);
+
+                case ServiceLifetime.Transient:
+                    return services.AddTransient<TService, TImplementation>(implementationFactory);
+
+                default:
+                    throw new ArgumentNullException(nameof(serviceLifetime));
+            }
+        }
+
+        //
+        // Summary:
+        //     Adds a transient service of the type specified in TService with a factory specified
+        //     in implementationFactory to the specified Microsoft.Extensions.DependencyInjection.IServiceCollection.
+        //
+        // Parameters:
+        //   services:
+        //     The Microsoft.Extensions.DependencyInjection.IServiceCollection to add the service
+        //     to.
+        //
+        //   implementationFactory:
+        //     The factory that creates the service.
+        //
+        // Type parameters:
+        //   TService:
+        //     The type of the service to add.
+        //
+        // Returns:
+        //     A reference to this instance after the operation has completed.
+        public static IServiceCollection Add<TService>(this IServiceCollection services, Func<IServiceProvider, TService> implementationFactory, ServiceLifetime serviceLifetime = ServiceLifetime.Transient)
+            where TService : class
+        {
+            switch (serviceLifetime)
+            {
+                case ServiceLifetime.Singleton:
+                    return services.AddSingleton<TService>(implementationFactory);
+
+                case ServiceLifetime.Scoped:
+                    return services.AddScoped<TService>(implementationFactory);
+
+                case ServiceLifetime.Transient:
+                    return services.AddTransient<TService>(implementationFactory);
+
+                default:
+                    throw new ArgumentNullException(nameof(serviceLifetime));
+            }
+        }
+
+        //
+        // Summary:
+        //     Adds a transient service of the type specified in TService to the specified Microsoft.Extensions.DependencyInjection.IServiceCollection.
+        //
+        // Parameters:
+        //   services:
+        //     The Microsoft.Extensions.DependencyInjection.IServiceCollection to add the service
+        //     to.
+        //
+        // Type parameters:
+        //   TService:
+        //     The type of the service to add.
+        //
+        // Returns:
+        //     A reference to this instance after the operation has completed.
+        public static IServiceCollection Add<TService>(this IServiceCollection services, ServiceLifetime serviceLifetime = ServiceLifetime.Transient)
+            where TService : class
+        {
+            switch (serviceLifetime)
+            {
+                case ServiceLifetime.Singleton:
+                    return services.AddSingleton<TService>();
+
+                case ServiceLifetime.Scoped:
+                    return services.AddScoped<TService>();
+
+                case ServiceLifetime.Transient:
+                    return services.AddTransient<TService>();
+
+                default:
+                    throw new ArgumentNullException(nameof(serviceLifetime));
+            }
+        }
+
+        //
+        // Summary:
+        //     Adds a transient service of the type specified in serviceType to the specified
+        //     Microsoft.Extensions.DependencyInjection.IServiceCollection.
+        //
+        // Parameters:
+        //   services:
+        //     The Microsoft.Extensions.DependencyInjection.IServiceCollection to add the service
+        //     to.
+        //
+        //   serviceType:
+        //     The type of the service to register and the implementation to use.
+        //
+        // Returns:
+        //     A reference to this instance after the operation has completed.
+        public static IServiceCollection Add(this IServiceCollection services, Type serviceType, ServiceLifetime serviceLifetime = ServiceLifetime.Transient)
+        {
+            switch (serviceLifetime)
+            {
+                case ServiceLifetime.Singleton:
+                    return services.AddSingleton(serviceType);
+
+                case ServiceLifetime.Scoped:
+                    return services.AddScoped(serviceType);
+
+                case ServiceLifetime.Transient:
+                    return services.AddTransient(serviceType);
+
+                default:
+                    throw new ArgumentNullException(nameof(serviceLifetime));
+            }
+        }
+
+        //
+        // Summary:
+        //     Adds a transient service of the type specified in TService with an implementation
+        //     type specified in TImplementation to the specified Microsoft.Extensions.DependencyInjection.IServiceCollection.
+        //
+        // Parameters:
+        //   services:
+        //     The Microsoft.Extensions.DependencyInjection.IServiceCollection to add the service
+        //     to.
+        //
+        // Type parameters:
+        //   TService:
+        //     The type of the service to add.
+        //
+        //   TImplementation:
+        //     The type of the implementation to use.
+        //
+        // Returns:
+        //     A reference to this instance after the operation has completed.
+        public static IServiceCollection Add<TService, TImplementation>(this IServiceCollection services, ServiceLifetime serviceLifetime = ServiceLifetime.Transient)
+            where TService : class
+            where TImplementation : class, TService
+        {
+            switch (serviceLifetime)
+            {
+                case ServiceLifetime.Singleton:
+                    return services.AddSingleton<TService, TImplementation>();
+
+                case ServiceLifetime.Scoped:
+                    return services.AddScoped<TService, TImplementation>();
+
+                case ServiceLifetime.Transient:
+                    return services.AddTransient<TService, TImplementation>();
+
+                default:
+                    throw new ArgumentNullException(nameof(serviceLifetime));
+            }
+        }
+
+        //
+        // Summary:
+        //     Adds a transient service of the type specified in serviceType with a factory
+        //     specified in implementationFactory to the specified Microsoft.Extensions.DependencyInjection.IServiceCollection.
+        //
+        // Parameters:
+        //   services:
+        //     The Microsoft.Extensions.DependencyInjection.IServiceCollection to add the service
+        //     to.
+        //
+        //   serviceType:
+        //     The type of the service to register.
+        //
+        //   implementationFactory:
+        //     The factory that creates the service.
+        //
+        // Returns:
+        //     A reference to this instance after the operation has completed.
+        public static IServiceCollection Add(this IServiceCollection services, Type serviceType, Func<IServiceProvider, object> implementationFactory, ServiceLifetime serviceLifetime = ServiceLifetime.Transient)
+        {
+            switch (serviceLifetime)
+            {
+                case ServiceLifetime.Singleton:
+                    return services.AddSingleton(serviceType, implementationFactory);
+
+                case ServiceLifetime.Scoped:
+                    return services.AddScoped(serviceType, implementationFactory);
+
+                case ServiceLifetime.Transient:
+                    return services.AddTransient(serviceType, implementationFactory);
+
+                default:
+                    throw new ArgumentNullException(nameof(serviceLifetime));
+            }
+        }
+
+        //
+        // Summary:
+        //     Adds a transient service of the type specified in serviceType with an implementation
+        //     of the type specified in implementationType to the specified Microsoft.Extensions.DependencyInjection.IServiceCollection.
+        //
+        // Parameters:
+        //   services:
+        //     The Microsoft.Extensions.DependencyInjection.IServiceCollection to add the service
+        //     to.
+        //
+        //   serviceType:
+        //     The type of the service to register.
+        //
+        //   implementationType:
+        //     The implementation type of the service.
+        //
+        // Returns:
+        //     A reference to this instance after the operation has completed.
+        public static IServiceCollection Add(this IServiceCollection services, Type serviceType, Type implementationType, ServiceLifetime serviceLifetime = ServiceLifetime.Transient)
+        {
+            switch (serviceLifetime)
+            {
+                case ServiceLifetime.Singleton:
+                    return services.AddSingleton(serviceType, implementationType);
+
+                case ServiceLifetime.Scoped:
+                    return services.AddScoped(serviceType, implementationType);
+
+                case ServiceLifetime.Transient:
+                    return services.AddTransient(serviceType, implementationType);
+
+                default:
+                    throw new ArgumentNullException(nameof(serviceLifetime));
+            }
+        }
+    }
+}
