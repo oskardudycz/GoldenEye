31b678cbe848d7b9ec275f0411801e1de095054c
MJendza <mateusz.jendza@euvic.pl>
2018-02-17 23:21:02 +0100

    #47 created dedicated Template/Template.DDD.WebApi project

---
+++ src/Template/Template.DDD.WebApi/DDD/Issues/IssueType.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend.DDD.Sample.Contracts.Issues
+{
+    public enum IssueType
+    {
+        UserStory = 1,
+        Task = 2,
+        Bug = 3
+    }
+}
---
+++ src/Template/Template.DDD.WebApi/DDD/Issues/Views/Issue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Backend.DDD.Sample.Contracts.Issues.Events;
+using GoldenEye.Backend.Core.DDD.Queries;
+using GoldenEye.Shared.Core.Objects.General;
+
+namespace Backend.DDD.Sample.Contracts.Issues.Views
+{
+    public class Issue : IView<Guid>
+    {
+        public Guid Id { get; set; }
+
+        public IssueType Type { get; private set; }
+
+        public string Title { get; private set; }
+
+        public string Description { get; private set; }
+
+        object IHasId.Id => Id;
+
+        public Issue()
+        {
+        }
+
+        public void Apply(IssueCreated @event)
+        {
+            Id = @event.IssueId;
+            Type = @event.Type;
+            Title = @event.Title;
+            Description = @event.Description;
+        }
+    }
+}
