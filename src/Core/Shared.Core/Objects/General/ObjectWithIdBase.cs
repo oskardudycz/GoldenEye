97bd7f9d951f0cb1d9065d7c5034868ea7ed589e
Oskar Dudycz <oskar.dudycz@gmail.com>
2018-03-12 23:21:00 +0100

    Added missing changes

---
+++ src/Core/Shared.Core/Objects/General/ObjectWithIdBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GoldenEye.Shared.Core.Objects.General
+{
+    public abstract class ObjectWithIdBase : IHasId
+    {
+        object IHasId.Id { get; }
+    }
+
+    public abstract class ObjectWithIdBase<T> : IHasId<T>
+    {
+        public T Id { get; set; }
+
+        object IHasId.Id
+        {
+            get { return Id; }
+        }
+    }
+
+    public abstract class ObjectWithGuidIdBase : ObjectWithIdBase<Guid>, IHasGuidId
+    {
+    }
+
+    public abstract class ObjectWithIntIdBase : ObjectWithIdBase<int>, IHasId
+    {
+    }
+}
