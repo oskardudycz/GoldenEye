7a8697c61413f3615e41d172e646c4fa8e794703
Oskar Dudycz <oskar.dudycz@gmail.com>
2018-04-19 21:19:25 +0200

    Added registration of configuration

---
+++ src/Core/Shared.Core/Configuration/Registration.cs
@@ -0,0 +1,16 @@
+﻿using GoldenEye.Shared.Core.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GoldenEye.Shared.Core.Configuration
+{
+    public static class Registration
+    {
+        public static void AddConfiguration<TConfiguation>(this IServiceCollection services, TConfiguation configuration, ServiceLifetime serviceLifetime = ServiceLifetime.Singleton)
+            where TConfiguation : class, IConfiguration
+        {
+            services.Add<IConfiguration, TConfiguation>(serviceLifetime);
+            services.Add<IConfiguration, TConfiguation>(sp => sp.GetService<TConfiguation>(), serviceLifetime);
+        }
+    }
+}
