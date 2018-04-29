f312cf4baf57359a079d1edd059b442e4c74d7be
Oskar Dudycz <oskar.dudycz@gmail.com>
2018-04-29 07:54:10 +0200

    Updated dapper data context tests

--- src/Core/Backend.Core.Dapper.Integration.Tests/Context/DapperDataContextTests.cs
+++ src/Core/Backend.Core.Dapper.Integration.Tests/Context/DapperDataContextTests.cs
@@ -1,4 +1,5 @@
-﻿using Backend.Core.Dapper.Integration.Tests.TestData;
+﻿using System.Linq;
+using Backend.Core.Dapper.Integration.Tests.TestData;
 using FluentAssertions;
 using GoldenEye.Backend.Core.Dapper.Context;
 using Marten.Integration.Tests.TestsInfrasructure;
@@ -35,6 +36,71 @@ namespace Backend.Core.Dapper.Integration.Tests.Context
             var recordFromDb = dataContext.GetById<User>(user.Id);
 
             recordFromDb.Should().BeEquivalentTo(result);
+
+            //3. Update
+            var userToUpdate = new User
+            {
+                Id = user.Id,
+                UserName = "tom.smith@mail.com",
+                FullName = "Tom Smith"
+            };
+
+            result = dataContext.Update(user);
+
+            result.Should().NotBe(null);
+            result.Id.Should().Be(user.Id);
+            result.UserName.Should().Be("tom.smith@mail.com");
+            result.FullName.Should().Be("Tom Smith");
+
+            recordFromDb = dataContext.GetById<User>(userToUpdate.Id);
+
+            recordFromDb.Should().BeEquivalentTo(result);
+
+            //4. Remove
+            result = dataContext.Remove(userToUpdate);
+
+            result.Should().NotBe(null);
+            result.Id.Should().Be(user.Id);
+            result.UserName.Should().Be("tom.smith@mail.com");
+            result.FullName.Should().Be("Tom Smith");
+
+            recordFromDb = dataContext.GetById<User>(userToUpdate.Id);
+
+            recordFromDb.Should().Be(null);
+
+            //5. Add Range
+
+            var results = dataContext.AddRange(
+                new User { UserName = "anna.frank@mail.com" },
+                new User { UserName = "anna.young@mail.com" },
+                new User { UserName = "anna.old@mail.com" }
+            )?.ToList();
+
+            results.Should().NotBeNull();
+            results.Should().HaveCount(3);
+
+            results[0].Id.Should().BeGreaterThan(0);
+            results[0].UserName.Should().Be("anna.frank@mail.com");
+            results[0].FullName.Should().BeNull();
+
+            results[1].Id.Should().BeGreaterThan(0);
+            results[1].UserName.Should().Be("anna.young@mail.com");
+            results[1].FullName.Should().BeNull();
+
+            results[2].Id.Should().BeGreaterThan(0);
+            results[2].UserName.Should().Be("anna.old@mail.com");
+            results[2].FullName.Should().BeNull();
+
+            //6. Query
+
+            var queryResults = dataContext.GetQueryable<User>().ToList();
+
+            queryResults.Should().Contain(x => results.Select(u => u.Id).Contains(x.Id));
+
+            queryResults = dataContext.GetQueryable<User>().Where(x => x.UserName == results[1].UserName).ToList();
+
+            queryResults.Should().HaveCountGreaterOrEqualTo(1);
+            queryResults.First(x => x.Id == results[1].Id).Should().BeEquivalentTo(results[1]);
         }
     }
 }
