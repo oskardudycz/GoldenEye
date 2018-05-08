986d23e04453fffb0c80bb3a3900d5f775cca4c0
Oskar Dudycz <oskar.dudycz@gmail.com>
2018-05-08 13:09:03 +0200

    Added basic integration test for template

---
+++ src/Template/GoldenEye.WebApi.SimpleDDD.IntegrationTests/Infrastructure/TestContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using GoldenEye.WebApi.SimpleDDD;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+
+namespace WebApi.SimpleDDD.IntegrationTests.Infrastructure
+{
+    public class TestContext : IDisposable
+    {
+        private TestServer _server;
+        public HttpClient Client { get; private set; }
+
+        public TestContext()
+        {
+            SetUpClient();
+        }
+
+        private void SetUpClient()
+        {
+            _server = new TestServer(new WebHostBuilder()
+                .UseStartup<Startup>());
+
+            Client = _server.CreateClient();
+        }
+
+        public void Dispose()
+        {
+            _server?.Dispose();
+            Client?.Dispose();
+        }
+    }
+}
---
+++ src/Template/GoldenEye.WebApi.SimpleDDD.IntegrationTests/Issues/IssueTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using FluentAssertions;
+using WebApi.SimpleDDD.IntegrationTests.Infrastructure;
+using Xunit;
+
+namespace WebApi.SimpleDDD.IntegrationTests.Issues
+{
+    public class IssueTests
+    {
+        private readonly TestContext _sut;
+
+        public IssueTests()
+        {
+            _sut = new TestContext();
+        }
+
+        [Fact]
+        public async Task IssueFlowTests()
+        {
+            var response = await _sut.Client.GetAsync("/api/Issues");
+
+            response.EnsureSuccessStatusCode();
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+    }
+}
