202c08e4ef364308229114a50bb582dd1deec7a2
Oskar Dudycz <oskar.dudycz@gmail.com>
2018-05-12 15:37:55 +0200

    Fixed wrong type of id in Delete method in IRepository and IRestService (#60)
    * Fixed wrong type of id in Delete method in IRepository and IRestService
    Added overloads with cancellation token only of async IRepository methods
    
    * Updated packages versions and Changelogs
    
    * Updated Changelogs and packages versions of Identity modules

--- src/Core/Backend.Core/Services/IRestService.cs
+++ src/Core/Backend.Core/Services/IRestService.cs
@@ -6,7 +6,7 @@ namespace GoldenEye.Backend.Core.Services
 {
     public interface IRestService<TDTO> : IReadonlyService<TDTO> where TDTO : class, IDTO
     {
-        Task<bool> DeleteAsync(int id, CancellationToken cancellationToken = default(CancellationToken));
+        Task<bool> DeleteAsync(object id, CancellationToken cancellationToken = default(CancellationToken));
 
         Task<TDTO> PostAsync(TDTO dto, CancellationToken cancellationToken = default(CancellationToken));
 
