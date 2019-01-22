6540795f720fc9a5676a925280df1c5fe8e66893
Oskar Dudycz <oskar.dudycz@gmail.com>
2019-01-22 22:14:34 +0100

    Updated to 2.2

--- src/Sample/GoldenEye.WebApi.Sample/Startup.cs
+++ src/Sample/GoldenEye.WebApi.Sample/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -34,9 +30,6 @@ namespace GoldenEye.WebApi.Sample
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
             app.UseMvc();
         }
     }
