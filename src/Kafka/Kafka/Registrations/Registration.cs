820ff170b1a7fbdba5f3e0e9822030814ed33f42
Faysal Aberkane <faysal.aberkane@gmail.com>
2021-01-26 10:35:48 +0000

    added fix for Kafka producer registration

--- src/Kafka/Kafka/Registrations/Registration.cs
+++ src/Kafka/Kafka/Registrations/Registration.cs
@@ -1,10 +1,10 @@
-using Core.Streaming.Kafka.Consumers;
 using GoldenEye.Events.External;
 using GoldenEye.Kafka.Consumers;
 using GoldenEye.Kafka.Producers;
 using GoldenEye.Registration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using System.Linq;
 
 namespace GoldenEye.Kafka.Registrations
 {
@@ -13,6 +13,10 @@ namespace GoldenEye.Kafka.Registrations
 
         public static IServiceCollection AddKafkaProducer(this IServiceCollection services)
         {
+            //removing NulloExternalEventProducer added by default
+            var serviceDescriptor = services.FirstOrDefault(descriptor => descriptor.ServiceType == typeof(IExternalEventProducer));
+            services.Remove(serviceDescriptor);
+
             //using TryAdd to support mocking, without that it won't be possible to override in tests
             services.TryAddScoped<IExternalEventProducer, KafkaProducer>();
             return services;
